1c2562dbc33e5e0dcf6c996b668b2214d4bc0b56
brandon <bjd2385.linux@gmail.com>
2018-06-26 14:40:09 -0400

    Stack implementation for interpreter

--- stack.c
+++ stack.c
@@ -7,4 +7,105 @@
 
 #include "stack.h"
 
+#include <stdio.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <string.h>
 
+Stack* 
+createStack(int size)
+{
+    Stack *stck;
+    stck = malloc(sizeof(Stack));
+    stck->count = 0;
+    stck->size = size;
+    stck->contents = malloc(size * sizeof(char));
+    memset(stck->contents, 0, size);
+    return stck;
+}
+
+void
+destroyStack(Stack* stack)
+{
+    free(stack->contents);
+    free(stack);
+}
+
+bool
+isEmpty(Stack* stack)
+{
+    return stack->count == 0;
+}
+
+bool
+isFull(Stack* stack)
+{
+    return stack->count == stack->size;
+}
+
+void
+push(Stack* stack, char character)
+{
+    // Push a character onto the stack
+    stack->contents[stack->count] = character;
+    stack->count++;
+}
+
+bool
+safepush(Stack* stack, char character)
+{
+    // If the stack is full, return false; else, return true
+    if (isFull(stack)) {
+        return false;
+    } else {
+        push(stack, character);
+        return true;
+    }
+}
+
+void
+safeExpandPush(Stack* stack, char character)
+{
+    // If the stack is full, double the usable heap
+    if (isFull(stack)) {
+        char* new_heap = malloc(stack->size * 2 * sizeof(char));
+        int i;
+        for (i = 0; i < stack->size; ++i)
+            new_heap[i] = stack->contents[i];
+        free(stack->contents);
+        stack->contents = new_heap;
+        stack->size <<= 1;
+    }
+    push(stack, character);
+}
+
+char
+pop(Stack* stack)
+{
+    // Pop a value off the stack.
+    char value = stack->contents[stack->count];
+    stack->contents[stack->count] = 0;  // overwrite data (optional)
+    stack->count--;
+    return value;
+}
+
+int
+main(int argc, char **argv)
+{
+    Stack* stack = createStack(5);
+    printf("Size: %d\n", stack->size);
+    int i, current_size = stack->size;
+    for (i = 0; i < current_size + (current_size / 2); ++i) 
+        safeExpandPush(stack, 'a');
+    printf("Size: %d\n", stack->size);
+    for (i = 0; i < stack->size; ++i) {
+        char character = stack->contents[i];
+        if (character == 0)
+            printf("Em ");
+        else
+            printf("%c ", character);
+    }
+    printf("\n");
+    destroyStack(stack);
+    return 0;
+}
---
+++ stack.h
@@ -0,0 +1,12 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct 
+{
+    int size;
+    int count;
+    char* contents;
+} Stack;
+
+
+#endif /* !STACK_H */
